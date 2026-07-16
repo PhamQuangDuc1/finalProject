1fab987871ae2689a6577ac02401d8827b6eadde
HoangTien-dot <HuynhTien2809202@gmail.com>
2026-07-16 16:06:51 +0700

    Update UI pages

--- finalProject/wwwroot/js/site.js
+++ finalProject/wwwroot/js/site.js
@@ -1,4 +1,11 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".studymate-marquee").forEach((marquee) => {
+    marquee.addEventListener("pointerenter", () => marquee.classList.add("is-paused"));
+    marquee.addEventListener("pointerleave", () => marquee.classList.remove("is-paused"));
+    marquee.addEventListener("focusin", () => marquee.classList.add("is-paused"));
+    marquee.addEventListener("focusout", () => marquee.classList.remove("is-paused"));
+  });
+});
